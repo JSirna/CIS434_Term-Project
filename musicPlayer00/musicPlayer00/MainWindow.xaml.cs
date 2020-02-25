a1c181e38d92cdce6a62ee780abbab43312e5bdf
JSirna <whitewolfhunter13@yahoo.com>
2020-02-25 13:36:11 -0500

    Changed gui - using WPF

---
+++ musicPlayer00/musicPlayer00/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+namespace musicPlayer00
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void OpenFolder() //refactor as needed
+        {
+
+        }
+
+        public void LoadMusic() //refactor as needed
+        {
+
+        }
+    }
+}
