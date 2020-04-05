bcfc790fb99350163773165fb82ef92b67658a86
Sean Surtz <Surtzsean@gmail.com>
2020-04-05 00:33:19 -0400

    Added Song and Playlist classes -Haven't implemented them into app yet -Going to use them to change how songs/playlists are stored -I didn't change anything in MainWindow but I'm getting a TagLib error

---
+++ CIS434_Term-Project-master/musicPlayer00/musicPlayer00/Song.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace musicPlayer00
+{
+    class Song
+    {
+        private Playlist pl;
+        private String name;
+        private int position;
+        public Song(Playlist pl, String name, int position)
+        {
+            this.pl = pl;
+            this.name = name;
+            this.position = position;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public Playlist getPlaylist()
+        {
+            return this.pl;
+        }
+        public int getPosition()
+        {
+            return this.position;
+        }
+        public void setPosition(int position)
+        {
+            this.position = position;
+        }
+    }
+}
